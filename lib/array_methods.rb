808edb3b6ea01db8e79d94fe30be187f38224455
Brian Rhodes <brianr4548@gmail.com>
2020-10-03 01:53:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,15 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+array2 = array.sort
+array2
 end
 
 def using_reverse(array)
-
+array2 = array.reverse
+array2
 end
 
 def using_first(array)
